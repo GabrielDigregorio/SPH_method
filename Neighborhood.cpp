cd25fab366a6dd5bd2ff88fe9d6839d04ea06b0a
gabriel dg <gabriel dg>
2017-02-15 19:24:38 +0100

    add Neighborhood.cpp and its reference in SPH.h

---
+++ Neighborhood.cpp
@@ -0,0 +1,44 @@
+#include "sph.h"
+
+
+
+/// Naive method to find the neighbors of a given particle at position p(x,y,z)
+
+void neighborNaive (double p[3], double h, std::vector<double> &pos, std::vector<double> &neighbor)
+{ 
+    // memory allocation
+    /*pos.reserve(pos.size() + ??? ); GABY : We don't know how many neighbors...*/
+
+    for(int i=0; i<=pos.size(); i=i+3)
+    {
+        if(pow((p[0]-pos[i]),2)+pow((p[1]-pos[i+1]),2)+pow((p[2]-pos[i+2]),2) <= pow(h,2))
+        {
+            // the vector neighbor take the pos vector indices where particles are close to p 
+            neighbor.push_back(i);
+        }
+
+    }
+
+}
+
+
+
+/// Advanced method to find the neighbors of a given particle at position p(x,y,z)
+
+// All-Pair search algorithm
+void neighborAllPair (double p[3], double h, std::vector<double> &pos)
+{
+
+}
+
+// Linked-list algorithm
+void neighborLinkedList (double p[3], double h, std::vector<double> &pos)
+{
+
+}
+
+// tree search algorithm
+void neighborTree (double p[3], double h, std::vector<double> &pos)
+{
+
+}
--- SPH.h
+++ SPH.h
@@ -8,8 +8,14 @@
 #include <cmath>
 #include <cassert>
 
+// Geometry.cpp
 void meshcube(double o[3], double L[3], double s, std::vector<double> &pos);
 void meshcube(double o[3], double L, double R, double s, std::vector<double> &pos);
+
+// Neighborhood
+void neighborNaive (double p[3], double h, std::vector<double> &pos, std::vector<double> &neighbor);
+
+// ParaView.cpp
 void paraview(std::string const &filename, 
               int step,
               std::vector<double> const &pos,
