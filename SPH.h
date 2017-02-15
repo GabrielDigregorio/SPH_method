726dc12919b4d9670976b021df5b1676da3f889a
gabriel dg <gabriel dg>
2017-02-15 19:42:27 +0100

    add comments

--- SPH.h
+++ SPH.h
@@ -10,7 +10,7 @@
 
 // Geometry.cpp
 void meshcube(double o[3], double L[3], double s, std::vector<double> &pos);
-void meshcube(double o[3], double L, double R, double s, std::vector<double> &pos);
+void meshcylinder(double o[3], double L, double R, double s, std::vector<double> &pos);
 
 // Neighborhood
 void neighborNaive (double p[3], double h, std::vector<double> &pos, std::vector<double> &neighbor);
