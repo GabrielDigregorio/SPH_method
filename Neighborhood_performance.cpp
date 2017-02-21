b055ee5359c5f21761a81201bf38119c6a5028fd
XavierAdriaens <xavier.adriaens@student.ulg.ac.be>
2017-02-21 17:53:28 +0100

    Ajout du tri Linked List(qui fonctionne)

--- Neighborhood_performance.cpp
+++ Neighborhood_performance.cpp
@@ -1,7 +1,3 @@
-/*
- * To be finished.
- */
-
 #include "SPH.h"
 #include <fstream>
 #include <vector>
@@ -12,10 +8,14 @@ int main(int argc, char *argv[])
     //Should be in argv[]
     double o[3] = {0.0,0.0,0.0};
     double L[3] = {10.0,10.0,10.0};
-    double s = 1.0;
-    //End input (end of what should be in argv
+    double s = 0.5;
+    double kh = 2.5;
+    //End input (end of what should be in argv)
 
     std::vector<double> pos;
+    std::vector<double> values;
+    std::vector<int> row;
+    std::vector<int> column;
 
     //Generate cube
     meshcube(o,L,s,pos);
@@ -25,7 +25,10 @@ int main(int argc, char *argv[])
     double duration;
     start = std::clock();
 
-    //Apply sorting
+    std::cout << "All Linked List :\n";
+    neighborLinkedList(pos,o, L, kh, values, row, column);
+    std::cout << "All pair :\n";
+    //neighborAllPair(pos, kh, values, row, column);
 
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
 
