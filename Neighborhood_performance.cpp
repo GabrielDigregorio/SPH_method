7a3fcda87fc796d74c0f1474c8ab796a8b126e40
gabriel dg <gabriel dg>
2017-02-22 17:22:01 +0100

    solve conflict Neighborhood_performance.cpp

--- Neighborhood_performance.cpp
+++ Neighborhood_performance.cpp
@@ -1,13 +1,5 @@
 #include "SPH.h"
-<<<<<<< HEAD
-#include <fstream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-=======
 
->>>>>>> 1216497b23b0f67d0cc2c237dd404a6cf84ef544
 
 int main(int argc, char *argv[])
 {
@@ -18,14 +10,7 @@ int main(int argc, char *argv[])
     std::cout << "\n Parameter list: " << s << ", " << kh << ", " << l << "\n";
 
     double o[3] = {0.0,0.0,0.0};
-<<<<<<< HEAD
     double L[3] = {l,l,l};
-=======
-    double L[3] = {10.0,10.0,10.0};
-    double s = 1.4;
-    double kh = 2.2;
-    //End input (end of what should be in argv)
->>>>>>> 1216497b23b0f67d0cc2c237dd404a6cf84ef544
 
     std::vector<double> pos;
     std::vector<double> values;
