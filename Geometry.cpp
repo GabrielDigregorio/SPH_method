64f8c14ec547ce4e9b55b58bf1d2ed6211453d6d
gabriel dg <gabriel dg>
2017-02-22 22:39:19 +0100

    centered cube and cylinder to (0,0,0)

--- Geometry.cpp
+++ Geometry.cpp
@@ -23,7 +23,7 @@ void meshcube(double o[3], double L[3], double s, std::vector<double> &pos, doub
     double dz = L[2]/nk; ++nk;
 
     // output
-    std::cout << "meshing cube at o=(" <<o[0]<< ","  <<o[1]<< ","  <<o[2]<< ") ";
+    std::cout << "meshing cube at center o=(" <<o[0]-L[0]/2<< ","  <<o[1]-L[1]/2<< ","  <<o[2]-L[2]/2<< ") ";
     std::cout << "of size L=(" <<L[0]<< ","  <<L[1]<< ","  <<L[2]<< ")\n";
     std::cout << "\tparticle spacing s=(" <<dx<< ","  <<dy<< ","  <<dz<< ") [target was s=" << s << "]\n";
     std::cout << "\t=> "<<ni<< "*"  <<nj<< "*"  <<nk<< " = " << ni*nj*nk << " particles to be generated\n";
@@ -38,13 +38,13 @@ void meshcube(double o[3], double L[3], double s, std::vector<double> &pos, doub
     // particle generation
     for(int k=0; k<nk; ++k)
     {
-        double z = o[2]+ k*dz;
+        double z = o[2] - L[2]/2 + k*dz;
         for(int j=0; j<nj; ++j)
         {
-            double y = o[1]+j*dy;
+            double y = o[1] - L[1]/2 +j*dy;
             for(int i=0; i<ni; ++i)
             {
-                double x = o[0]+i*dx;
+                double x = o[0] - L[0]/2 +i*dx;
                 pos.push_back(x + distribution(generator));
                 pos.push_back(y + distribution(generator));
                 pos.push_back(z + distribution(generator));
@@ -59,25 +59,25 @@ void meshcube(double o[3], double L[3], double s, std::vector<double> &pos, doub
 
 // build a cylinder of particles with the center of the lower base aligned with x,y,z axes.
 //  - o[3]: center of the cylinder
-//  - R: radius of the cylinder
-//  - L: length of the cylinder
+//  - L[3]: diameter1 of the cylinder, diameter2 of the cylinder, length of the cylinder
 //  - s: particle spacing
+//  - pertubation: percentage of perturbation in position of particles (equal 0 by default)
 
-void meshcylinder(double o[3], double L, double R, double s, std::vector<double> &pos, double perturbation)
+void meshcylinder(double o[3], double L[3], double s, std::vector<double> &pos, double perturbation)
 {
     // open a file to write the geometry (check for valydity) MUST BE REMOVE LATER
     ofstream myfile;
     myfile.open ("cylinder.txt");
 
     // calculate nb of particles along the radius from target size "s"
-    int nl = int(ceil(L/s));
-    double dl = L/nl; ++nl;
-    int nr = int(ceil(R/s));
-    double dr = R/nr; ++nr;
+    int nl = int(ceil(L[2]/s));
+    double dl = L[2]/nl; ++nl;
+    int nr = int(ceil(L[0]/(2*s)));
+    double dr = L[0]/(2*nr); ++nr;
 
     // output
     std::cout << "meshing cylinder at o=(" <<o[0]<< ","  <<o[1]<< ","  <<o[2]<< ") ";
-    std::cout << "of radius R=" <<R<< " and L=" <<L<< "\n";
+    std::cout << "of diameter D1=" << L[0] << ", diameter D2=" << L[1] << " and L=" << L[2] << "\n";
     std::cout << "\tparticle spacing s=(" <<dr<< "," <<dl<< ") [target was s=" << s << "]\n";
     std::cout << "\t=> "<<nl<< "*"  <<nr<< "*"  <<nr<< " = " << nl*nr*nr << " particles to be generated\n";
 
@@ -89,13 +89,13 @@ void meshcylinder(double o[3], double L, double R, double s, std::vector<double>
     std::uniform_real_distribution<double> distribution(-s*perturbation/100,s*perturbation/100);
 
     // particle generation
-    for(int l=0; l<nl-1; ++l)
+    for(int l=(-nl+1)/2; l<=(nl-1)/2; ++l)
     {
         double z = o[2]+ l*dl;
         for(int i=-nr+1; i<nr; ++i)
         {
             double x = o[0]+i*dr;
-            for(int j=-int(sqrt(pow(R,2)-pow(x,2))); j<int(sqrt(pow(R,2)-pow(x,2)))+1; ++j)
+            for(int j=-int(sqrt(pow(L[1]/2,2)-pow(x,2))); j<int(sqrt(pow(L[1]/2,2)-pow(x,2)))+1; ++j)
             {
                 double y = o[1]+j*dr;
                 pos.push_back(x + distribution(generator));
--- MeshGeometry.m
+++ MeshGeometry.m
@@ -2,6 +2,7 @@
 A = importdata('cube.txt');
 
 scatter3(A(:,1),A(:,2),A(:,3))
+axis([-20 20 -20 20 -20 20])
 
 
 
