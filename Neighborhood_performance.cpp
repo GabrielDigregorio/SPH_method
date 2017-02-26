7b000e2c5a45d3fad8f1896f4ed243e87e30a3a1
XavierAdriaens <xavier.adriaens@student.ulg.ac.be>
2017-02-26 15:00:22 +0100

    Change to allow performance testing

--- Neighborhood_performance.cpp
+++ Neighborhood_performance.cpp
@@ -4,22 +4,22 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
-using namespace std;
 
 int main(int argc, char *argv[])
 {
     // open a file to write the time (import into matlab) MUST BE REMOVE LATER
-    ofstream myfile;
-    myfile.open("neighborAnalysis.txt", std::ofstream::out | std::ofstream::app);
+    //ofstream myfile;
+    //myfile.open("neighborAnalysis.txt", std::ofstream::out | std::ofstream::app);
 
     // increment the length of the cube
-    for(int i=1 ; i<=11 ; ++i)
-    {
+    //for(int i=1 ; i<=11 ; ++i)
+    //{
         //Input parameters
         double s = atof(argv[1]);
         double kh = atof(argv[2]);
-        double l = atof(argv[3]) + i;
-        std::cout << "\n Parameter list: " << s << ", " << kh << ", " << l << "\n";
+        double l = atof(argv[3]);
+        double eps = atof(argv[4]);
+        std::cout << "\n Parameter list: " << s << ", " << kh << ", " << l << ", " << eps << "\n";
 
         double o[3] = {0.0,0.0,0.0};
         double L[3] = {l,l,l};
@@ -30,8 +30,11 @@ int main(int argc, char *argv[])
         std::vector<int> column;
 
         //Generate cube
-        meshcube(o,L,s,pos, 0);
-        myfile << (l+1)*(l+1)*(l+1) << " " ;
+        meshcube(o,L,s,pos, eps);
+        //myfile << (l+1)*(l+1)*(l+1) << " " ;
+
+        double ll[3] = {-L[0]/2, -L[1]/2, -L[2]/2};
+        double uu[3] = {L[0]/2, L[1]/2, L[2]/2};
 
         //Record algorithm performance
         std::clock_t start;
@@ -41,18 +44,16 @@ int main(int argc, char *argv[])
         neighborAllPair(pos, kh, values, row, column);
         duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
         std::cout<<"Elapsed time AllPair: "<< duration <<" [s]\n";
-        myfile << duration << " " ;
+        //myfile << duration << " " ;
 
         start = std::clock();
-        double ll[3] = {-L[0]/2, -L[1]/2, -L[2]/2};
-        double uu[3] = {L[0]/2, L[1]/2, L[2]/2};
         neighborLinkedList(pos, ll, uu, kh, values, row, column);
         duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
         std::cout<<"Elapsed time Linked List: "<< duration <<" [s]\n";
-        myfile << duration << " " << "\n" ;
-    }
+        //myfile << duration << " " << "\n" ;
+    //}
 
     std::cout << "\n";
-    myfile.close();
+    //myfile.close();
     return 0;
 }
