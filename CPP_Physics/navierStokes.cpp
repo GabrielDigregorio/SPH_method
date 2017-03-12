5c1f2cf202db52d0a072aede585144c2eb819f05
juliendular <julien.dular@student.ulg.ac.be>
2017-03-12 14:52:15 +0100

    to pull modif...

--- CPP_Physics/navierStokes.cpp
+++ CPP_Physics/navierStokes.cpp
@@ -5,17 +5,15 @@ double continuity(int particleID, std::vector<int>& neighbors, std::vector<doubl
 {
   double densityDerivative = 0.0;
   double scalarProduct = 0.0;
-  for (int i = 0; i < neighbors.size(); i++)
-  {
+  for (int i = 0; i < neighbors.size(); i++){
     //Compute scalar product present in the formula
-    for (int j = 0; j <= 2; j++)
-    {
-      scalarProduct += (currentField->speed[3*particleID + j] - currentField->speed[3*i + j])*(currentField->pos[3*particleID + j] - currentField->pos[3*i + j]);
+    for (int j = 0; j <= 2; j++){
+      scalarProduct += (currentField->speed[3*particleID + j] - currentField->speed[3*neighbors[i] + j])
+            * kernelGradients[3*i + j];
     }
     densityDerivative += currentField->mass[neighbors[i]]  * scalarProduct * kernelGradients[i]; //TO BE VERIFIED TO BE COHERENT WITH kernelGradients !!
   }
   return densityDerivative;
-
 }
 
 
@@ -26,13 +24,14 @@ void momentum(int particleID, std::vector<int>& neighbors, std::vector<double>& 
   std::vector<double> viscosity;
 
   // ATTENTION !!! ON DOIT ENVOYER LA VRAI VITESSE DU SON ET LE VRAI H !!! (j'ai mis en attendant, 300m/s et h=kh/2')
-  viscosityComputation(particleID, neighbors, currentField, parameter, viscosity, 300.0, parameter->kh/2); 
+  viscosityComputation(particleID, neighbors, currentField, parameter, viscosity, 300.0, parameter->kh/2);
 
   for (int j = 0; j <= 2; j++)
   {
     for (int i = 0; i < neighbors.size(); i++)
     {
-      speedDerivative[j] -= currentField->mass[neighbors[i]] * ( currentField->pressure[neighbors[i]]/((currentField->density[neighbors[i]]*(currentField->density[neighbors[i]]))) + currentField->pressure[particleID]/((currentField->density[particleID]*(currentField->density[particleID]))) + viscosity[i] ) * (currentField->pos[3*particleID + j] - currentField->pos[3*neighbors[i] + j]) * kernelGradients[i];
+      speedDerivative[j] -= currentField->mass[neighbors[i]] * ( currentField->pressure[neighbors[i]]/((currentField->density[neighbors[i]]*(currentField->density[neighbors[i]]))) + currentField->pressure[particleID]/((currentField->density[particleID]*(currentField->density[particleID]))) + viscosity[i] )
+      * (currentField->pos[3*particleID + j] - currentField->pos[3*neighbors[i] + j]) * kernelGradients[i];
     }
   }
   speedDerivative[3] -= parameter->g;
