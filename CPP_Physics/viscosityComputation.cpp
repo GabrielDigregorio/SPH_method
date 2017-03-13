abf088d09663f304b103e8e6a74fc4e7a6f3f327
firerom <romain63@hotmail.com>
2017-03-13 17:30:44 +0100

    correction viscosité

--- CPP_Physics/viscosityComputation.cpp
+++ CPP_Physics/viscosityComputation.cpp
@@ -12,26 +12,24 @@ void viscosityComputation(int particleID, std::vector<int>& neighbors, Field* cu
   switch(parameter->viscosityModel){
   case violeauArtificial :
       // For each neighbor of the particleID
-      /* FAUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUX !!!!!!!!!!
       for (int i = 0; i < neighbors.size(); i++)
       {
-          double U_ij = currentField->speed[neighbors[i]] * currentField->speed[particleID]
-                + currentField->speed[neighbors[i]] * currentField->speed[particleID]
-                + currentField->speed[neighbors[i]]*currentField->speed[particleID]; // velocity
-          double R_ij = sqrt(distance(currentField->pos, particleID, neighbors[i])); // distance
-
-          if(U_ij * R_ij < 0)
-          {
-            double rho  = 0.5 * (currentField->density[particleID] + currentField->density[neighbors[i]]); // average
-            double nu2  = epsilon*h*h;
-            double mu   = (h*U_ij*R_ij) / (R_ij*R_ij + nu2);
-
-            viscosity[i] = ( -alpha*c*mu + beta*mu*mu ) / (rho);
-          }
-          else viscosity[i] = 0.0;
+        double ux =  currentField->speed[3*particleID]-currentField->speed[3*neighbors[i]];
+        double uy =  currentField->speed[3*particleID+1]-currentField->speed[3*neighbors[i]+1];
+        double uz =  currentField->speed[3*particleID+2]-currentField->speed[3*neighbors[i]+2];
+        double rx =  currentField->pos[3*particleID]-currentField->pos[3*neighbors[i]];
+        double ry =  currentField->pos[3*particleID+1]-currentField->pos[3*neighbors[i]+1];
+        double rz =  currentField->pos[3*particleID+2]-currentField->pos[3*neighbors[i]+2];
+        double Rij_Uij = ux*rx+ry*uy+rz*uz;// moyen plus élégant de faire tout ça ?
+        if(Rij_Uij < 0)
+        {
+          double Rij2 = rx*rx+ry*ry+rz*rz;
+          double nu2 =epsilon*h*h;
+          double mu = (h*Rij_Uij)/(Rij2+nu2);
+          double rho  = 0.5 * (currentField->density[particleID] + currentField->density[neighbors[i]]);
+          viscosity[i] = ( -alpha*c*mu + beta*mu*mu ) / (rho);
+        }
       }
-      */
-
     break;
 
     default :
