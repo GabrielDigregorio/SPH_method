5fa4c9171a84fd17abb2ed2bcbe6d32eb17b628a
juliendular <julien.dular@student.ulg.ac.be>
2017-04-04 11:23:56 +0200

    test on new branch

--- CPP_Main/SPH.cpp
+++ CPP_Main/SPH.cpp
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 	if(procID==0){writeField(globalField, 0.0, parameter, parameterFilename, geometryFilename, experimentFilename);}
 	unsigned int writeCount = 1;
 	// Scatters the globalField from node 0 into the currentField of all nodes
-	scatterField(globalField, currentField);
+	scatterField(globalField, currentField); // will really start
 
 	// Copies the invariant information about the field
 	copyField(currentField, nextField);
